b082b22dbc28d0a6820ecf943d291e98e240c66a
Martizong <agregjtrestrjjs@gmail.com>
2023-12-23 15:42:16 +0200

    chore: fixed the total number to two digits

--- src/components/Info/index.jsx
+++ src/components/Info/index.jsx
@@ -4,7 +4,7 @@ export default function Info({ name, total, perc }) {
     return (
         <div className="infobox bg-dark-subtle rounded">
             <div className="fs-7 text-primary">{name}</div>
-            <div className="fs-3 text-light-emphasis">{total}</div>
+            <div className="fs-3 text-light-emphasis">{total.toFixed(2)}</div>
             {Boolean(perc) && (
                 <div className="fs-6 text-light-emphasis">{perc}%</div>
             )}
